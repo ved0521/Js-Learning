b9eecf899de0b234280c122cbd8e5ad67d53cecb
VedikaSingh <145755285+ved0521@users.noreply.github.com>
2023-10-27 11:57:17 +0000

    dataTypes

---
+++ Basic/dataType.js
@@ -0,0 +1,26 @@
+"use Strict;"//treat all Js code as newer version
+
+//alert("hello")// we are using nodejs,not browser
+
+console.log(3
+    +3) //code readability should be high 
+console.log("vedika")
+
+let name = "vedika" //string//
+let age = 18  //int//
+let isLoggedIn = false //"Boolean"//
+let State; //undefine//
+
+//number  => 2 to pwer 53 
+//bigint 
+//string => use to ''," "
+//boolean => true/false
+// null => standalone value
+// undefined => the value is not assign.
+//symbol => unique
+
+//object
+
+console.log(typeof age);// output is number
+console .log(typeof null);// output is object
+console .log(typeof undefined);// output is undefine
