6cc77f8575ed1d147518e4aa8ce48a8109bd6501
VedikaSingh <145755285+ved0521@users.noreply.github.com>
2023-11-02 10:09:10 +0000

    operations

---
+++ Basic/conversionoperation.js
@@ -0,0 +1,45 @@
+let score = "43"
+
+//console.log(typeof score);
+console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+///console.log(typeof valueInNumber);
+//console.log (valueInNumber)
+//"33" => 33
+//"33abc" => NaN
+//True => 1; false => 0
+
+let isLoggedIn =""
+
+let booleanIs
+
+//1 => true;0=>false
+//""=>false
+//"vedika"=>true
+
+let someNumber =33
+
+let stringNumber =String (someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//****************************** Operations ***********************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue );
+//console.log(2+2);
+//console.log(2-2);
+
+//trickey things//
+
+let str1 = "hello"
+let str2 ="vedika"
+
+let str3 =str1 +str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+2);
